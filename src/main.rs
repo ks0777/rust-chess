2012257cb51998d73065f54fb3a624f4a9b6aca6
Kevin Schneider <kevin.schneider@tum.de>
2021-09-19 11:50:39 +0200

    initial files

---
+++ src/main.rs
@@ -0,0 +1,223 @@
+use ggez::*;
+use ggez::graphics::Text;
+use ggez::graphics::Color;
+use ggez::input;
+use mint::{Point2, Vector2};
+
+#[derive(Clone, Copy, PartialEq)]
+enum FigureColor {
+    BLACK,
+    WHITE,
+    NONE
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum FigureType {
+    KING,
+    QUEEN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    PAWN,
+    NONE
+}
+
+struct Figure {
+    image: graphics::Image,
+    figure_type: FigureType,
+    figure_color: FigureColor,
+}
+
+#[derive(Clone, Copy)]
+struct Field {
+    figure_type: FigureType,
+    figure_color: FigureColor,
+}
+
+struct Board {
+    fields: [Field; 64],    
+}
+
+struct State {
+    dt: std::time::Duration, 
+    board: Board,    
+    figures: [Figure; 13],
+    drag_field: i8
+}
+
+impl State {
+    fn new(ctx: &mut Context) -> GameResult<State> {
+        println!("Creating figures");
+        let figures: [Figure; 13] = [
+            Figure { image: graphics::Image::new(ctx, "/king_b.png").unwrap(), figure_type: FigureType::KING, figure_color: FigureColor::BLACK },
+            Figure { image: graphics::Image::new(ctx, "/queen_b.png").unwrap(), figure_type: FigureType::QUEEN, figure_color: FigureColor::BLACK },
+            Figure { image: graphics::Image::new(ctx, "/bishop_b.png").unwrap(), figure_type: FigureType::BISHOP, figure_color: FigureColor::BLACK },
+            Figure { image: graphics::Image::new(ctx, "/knight_b.png").unwrap(), figure_type: FigureType::KNIGHT, figure_color: FigureColor::BLACK },
+            Figure { image: graphics::Image::new(ctx, "/rook_b.png").unwrap(), figure_type: FigureType::ROOK, figure_color: FigureColor::BLACK },
+            Figure { image: graphics::Image::new(ctx, "/pawn_b.png").unwrap(), figure_type: FigureType::PAWN, figure_color: FigureColor::BLACK },
+            Figure { image: graphics::Image::new(ctx, "/king_w.png").unwrap(), figure_type: FigureType::KING, figure_color: FigureColor::WHITE },
+            Figure { image: graphics::Image::new(ctx, "/queen_w.png").unwrap(), figure_type: FigureType::QUEEN, figure_color: FigureColor::WHITE },
+            Figure { image: graphics::Image::new(ctx, "/bishop_w.png").unwrap(), figure_type: FigureType::BISHOP, figure_color: FigureColor::WHITE },
+            Figure { image: graphics::Image::new(ctx, "/knight_w.png").unwrap(), figure_type: FigureType::KNIGHT, figure_color: FigureColor::WHITE },
+            Figure { image: graphics::Image::new(ctx, "/rook_w.png").unwrap(), figure_type: FigureType::ROOK, figure_color: FigureColor::WHITE },
+            Figure { image: graphics::Image::new(ctx, "/pawn_w.png").unwrap(), figure_type: FigureType::PAWN, figure_color: FigureColor::WHITE },
+            Figure { image: graphics::Image::solid(ctx, 1, Color::from_rgba(0,0,0,0)).unwrap(), figure_type: FigureType::NONE, figure_color: FigureColor::NONE },
+        ];
+        println!("done!");
+
+        // Set up board
+        let mut board: Board = Board { fields: [Field {figure_type: FigureType::NONE, figure_color: FigureColor::NONE}; 64] };
+
+        board.fields[0] = Field {figure_type: FigureType::ROOK, figure_color: FigureColor::BLACK};
+        board.fields[1] = Field {figure_type: FigureType::KNIGHT, figure_color: FigureColor::BLACK};
+        board.fields[2] = Field {figure_type: FigureType::BISHOP, figure_color: FigureColor::BLACK};
+        board.fields[3] = Field {figure_type: FigureType::QUEEN, figure_color: FigureColor::BLACK};
+        board.fields[4] = Field {figure_type: FigureType::KING, figure_color: FigureColor::BLACK};
+        board.fields[5] = Field {figure_type: FigureType::BISHOP, figure_color: FigureColor::BLACK};
+        board.fields[6] = Field {figure_type: FigureType::KNIGHT, figure_color: FigureColor::BLACK};
+        board.fields[7] = Field {figure_type: FigureType::ROOK, figure_color: FigureColor::BLACK};
+
+        board.fields[56] = Field {figure_type: FigureType::ROOK, figure_color: FigureColor::WHITE};
+        board.fields[57] = Field {figure_type: FigureType::KNIGHT, figure_color: FigureColor::WHITE};
+        board.fields[58] = Field {figure_type: FigureType::BISHOP, figure_color: FigureColor::WHITE};
+        board.fields[59] = Field {figure_type: FigureType::QUEEN, figure_color: FigureColor::WHITE};
+        board.fields[60] = Field {figure_type: FigureType::KING, figure_color: FigureColor::WHITE};
+        board.fields[61] = Field {figure_type: FigureType::BISHOP, figure_color: FigureColor::WHITE};
+        board.fields[62] = Field {figure_type: FigureType::KNIGHT, figure_color: FigureColor::WHITE};
+        board.fields[63] = Field {figure_type: FigureType::ROOK, figure_color: FigureColor::WHITE};
+
+        for i in 8..16 {
+            board.fields[i] = Field {figure_type: FigureType::PAWN, figure_color: FigureColor::BLACK};
+        }
+        for i in 48..56 {
+            board.fields[i] = Field {figure_type: FigureType::PAWN, figure_color: FigureColor::WHITE};
+        }
+
+
+        let s = State {
+            dt: std::time::Duration::new(0,0),
+            board : board,
+            figures: figures,
+            drag_field: -1
+        };
+        Ok(s)
+    }
+}
+
+const CHECKER_1: Color = Color{r: 0.431, g: 0.313, b: 0.313, a: 1.0};
+const CHECKER_2: Color = Color{r: 0.878, g: 0.756, b: 0.756, a: 1.0};
+
+fn draw_board(ctx: &mut Context, board: &Board, figures: &[Figure; 13], drag_field: i8) {
+        for i in 0..8 {
+           for j in 0..8 {
+                let color = if (i+j) % 2 != 0 { CHECKER_1 } else { CHECKER_2 };
+                let rectangle = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), graphics::Rect::new(i as f32 * 100.0, j as f32 * 100.0, 100.0, 100.0), color);
+                graphics::draw(ctx, &rectangle.unwrap(), graphics::DrawParam::default()).unwrap();
+
+                let field = &board.fields[i+j*8];
+                if field.figure_type != FigureType::NONE {
+                    for figure in figures {
+                        if figure.figure_type == field.figure_type && figure.figure_color == field.figure_color {
+                            let figure_dst = Point2 { x: i as f32 * 100.0, y: j as f32 * 100.0};
+                            let mut draw_param = graphics::DrawParam::default().dest(figure_dst).scale(Vector2{x: 0.09765625, y: 0.09765625});
+
+                            // draw shadow for dragged figure
+                            if i + j*8 == drag_field as usize {
+                                draw_param = draw_param.color(Color::from_rgba(0,0,0,160));
+                            }
+
+                            graphics::draw(ctx, &figure.image, draw_param).unwrap();
+                        }
+                    }
+                }
+           } 
+        }
+}
+
+fn draw_floating_figure(ctx: &mut Context, board: &Board, figures: &[Figure; 13], drag_field: i8) {
+    if drag_field != -1 {
+        input::mouse::set_cursor_hidden(ctx, true);
+        let field = &board.fields[drag_field as usize];    
+        for figure in figures {
+            if figure.figure_type == field.figure_type && figure.figure_color == field.figure_color {
+                let mut mouse_position = input::mouse::position(ctx);
+                mouse_position.x -= 50.0;
+                mouse_position.y -= 50.0;
+                graphics::draw(ctx, &figure.image, graphics::DrawParam::default().dest(mouse_position).scale(Vector2{x: 0.09765625, y: 0.09765625})).unwrap();
+            }
+        }
+    } else {
+        input::mouse::set_cursor_hidden(ctx, false);
+    }
+}
+
+impl ggez::event::EventHandler<GameError> for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        self.dt = timer::delta(ctx);
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, Color::BLACK);
+
+
+        draw_board(ctx, &self.board, &self.figures, self.drag_field);
+
+        draw_floating_figure(ctx, &self.board, &self.figures, self.drag_field);
+
+        let text = Text::new(format!("{} FPS", 1000 / self.dt.as_millis()));
+        let text_dst = Point2 { x: 10.0, y: 10.0};
+        graphics::draw(ctx, &text, graphics::DrawParam::default().dest(text_dst))?;
+
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        _btn: event::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        let drag_field = (((x as i32 - (x as i32 % 100)) / 100) + ((y as i32 - (y as i32 % 100)) / 100) * 8) as i8;
+        if self.board.fields[drag_field as usize].figure_type != FigureType::NONE {
+            self.drag_field = drag_field;
+        }
+    }
+
+    fn mouse_button_up_event(
+        &mut self,
+        _ctx: &mut Context,
+        _btn: event::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        if self.drag_field == -1 { return; }
+
+        let target_field = ((x as i32 - (x as i32 % 100)) / 100) + ((y as i32 - (y as i32 % 100)) / 100) * 8;
+        
+        if self.drag_field != target_field as i8 {
+            self.board.fields[target_field as usize] = self.board.fields[self.drag_field as usize];
+            self.board.fields[self.drag_field as usize] = Field { figure_type: FigureType::NONE, figure_color: FigureColor::NONE };
+        }
+        
+        self.drag_field = -1;
+    }
+
+}
+
+pub fn main() -> GameResult {
+    let c = conf::Conf::default().window_mode(conf::WindowMode::default().dimensions(800.0, 800.0));
+
+    let (mut ctx, event_loop) = ContextBuilder::new("hello_ggez", "Was weiss ich")
+        .default_conf(c)
+        .window_setup(
+            conf::WindowSetup::default().samples(conf::NumSamples::Eight))
+        .build()
+        .unwrap();
+
+    let state = State::new(&mut ctx)?;
+    event::run(ctx, event_loop, state);
+}
