3dbb4d3078c6878e726d0a60083693ddc295950d
Kevin Schneider <kevin.schneider@tum.de>
2021-10-04 08:57:18 +0200

    add diff script for perft tests

---
+++ scripts/diff.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+depth=1
+
+fen="r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
+fen="3k2r/p1ppqpb1/bn1Ppnp1/4N3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R b KQkq - 0 1"
+output=$(echo -e 'position fen '$fen'\ngo perft '$depth | stockfish)
+output=$(echo "$output" | sed '/^$/d')
+output=$(echo "$output" | sed '$d')
+output=$(echo "$output" | sort)
+#echo "$output"
+
+
+output2=$(../target/release/deps/chess-dd917230f7749151)
+output2=$(echo "$output2" | sed '1,10d')
+output2=$(echo "$output2" | sed '/^$/d')
+output2=$(echo "$output2" | sed '$d')
+output2=$(echo "$output2" | sed '$d')
+output2=$(echo "$output2" | sed '$d')
+output2=$(echo "$output2" | sed '$d')
+output2=$(echo "$output2" | sed '$d')
+output2=$(echo "$output2" | sed '$d')
+output2=$(echo "$output2" | sed '$d')
+output2=$(echo "$output2" | sed '$d')
+output2=$(echo "$output2" | sort)
+#echo "$output2"
+
+vimdiff <(echo "$output") <(echo "$output2")
